557c9b64b74746c9c4bd77e90b1a480626c3ce45
Andrea Martin <amartin.onda@gmail.com>
2022-04-08 10:47:01 +0200

    Add method container to substitute bottle(s), verse 2 and verse default are identical

--- lib/Bottles.rb
+++ lib/Bottles.rb
@@ -17,19 +17,27 @@ class Bottles
             "Take it down and pass it around, " +
             "no more bottles of beer on the wall.\n"
         when 2
-            "2 bottles of beer on the wall, " +
-            "2 bottles of beer.\n" +
+            "#{number} bottles of beer on the wall, " +
+            "#{number} bottles of beer.\n" +
             "Take one down and pass it around, " +
-            "1 bottle of beer on the wall.\n"
+            "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
         else
             "#{number} bottles of beer on the wall, " +
             "#{number} bottles of beer.\n" +
             "Take one down and pass it around, " +
-            "#{number - 1} bottle#{'s' unless (number - 1) == 1} of beer on the wall.\n"
+            "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
         end
     end
 
     def verses(starting,ending)
         starting.downto(ending).collect{|i| verse(i)}.join("\n")
+    end
+
+    def container(number=:FIXME)
+        if number == 1
+            "bottle"
+        else
+            "bottles"
+        end
     end
 end
