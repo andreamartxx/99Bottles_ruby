588cf7583f5717e95b7f5147e984d491a2a59e56
Andrea Martin <amartin.onda@gmail.com>
2022-04-08 10:25:05 +0200

    Interpolated bottle(s) and running test

--- spec/bottles_test.rb
+++ spec/bottles_test.rb
@@ -22,7 +22,7 @@ class BottlesTest < Minitest::Test
         expected = "2 bottles of beer on the wall, " +
             "2 bottles of beer.\n" +
             "Take one down and pass it around, " +
-            "1 bottles of beer on the wall.\n"
+            "1 bottle of beer on the wall.\n"
         assert_equal expected, Bottles.new.verse(2)
     end
         
